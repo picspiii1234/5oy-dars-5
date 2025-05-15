18aca7f7f413f4fa90f643395fad5904e3b95e2c
picspiii1234 <ibrihim539@gmail.com>
2025-05-15 21:41:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let ism = prompt('Ismingizni kiritin...')
+let spacid = prompt('Spays id kiriting...')
+let spesePasvord = prompt('SpaysPasvord kiriting...')
+
+
+let presen ={
+    name:ism,
+    id:spacid,
+    pasvord:spesePasvord,
+}
+
+console.log(presen);
